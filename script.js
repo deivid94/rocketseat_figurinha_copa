407776ebda7951e0696ce03a7349c94a09c06c04
Deivid Martins <md031194@gmail.com>
2022-10-31 23:55:28 -0300

    Onclick trocar classe Cards Implementada

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function changeCard()
 {
-  let trocarCard = document.getElementById('card').className = 'colorCard1'
-   if(trocarCard){
-    trocarCard.className = 'colorCard2'
-   }else{
-    trocarCard.className = 'colorCard1'
-   }
-   alert(trocarCard)
+  let trocarCard = document.getElementById('card')
+  if (trocarCard.classList.contains("colorCard2")){
+    trocarCard.classList.remove('colorCard2')
+    trocarCard.classList.add('colorCard1')
+  }else {
+    trocarCard.classList.remove('colorCard1')
+    trocarCard.classList.add('colorCard2')
+  }
 }
